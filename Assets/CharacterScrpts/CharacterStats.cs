a877b4101e16072a6960e032f67895dc4d7d81cf
Kahf Hussain <kahfnhussain@gmail.com>
2022-11-13 14:19:26 -0500

    Setting up Character Stats

--- Assets/CharacterScrpts/CharacterStats.cs
+++ Assets/CharacterScrpts/CharacterStats.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
+    public int health{get;private set;} = GameObject.GetComponent.CharacterSheet<vitality>;
+    public int food{get; private set;}
+    public int medicine{get; private set;} 
+    public float money{get; private set;} = 0f;
+    public int challengeScore{get; private set;} 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
