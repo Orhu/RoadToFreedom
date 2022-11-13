429bdd5b28619b57f27743c9a79abac2c5c3e756
Kahf Hussain <kahfnhussain@gmail.com>
2022-11-13 16:42:16 -0500

    Adding 'Status' List and Dictionary

--- Assets/CharacterScrpts/CharacterStats.cs
+++ Assets/CharacterScrpts/CharacterStats.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 public class CharacterStats : MonoBehaviour
 {
     public int health{get;private set;} = GameObject.GetComponent.CharacterSheet<vitality>;
-    public int food{get; private set;}
-    public int medicine{get; private set;} 
+    public int food{get; private set;} = 0;
+    public int medicine{get; private set;} = 0;
     public float money{get; private set;} = 0f;
-    public int challengeScore{get; private set;}
+    public int challengeScore{get; private set;} = 0;
+
+    public List<string> status = new List<string>();  
+    public IDictionary<string, string> statusDescription = new IDictionary<string, string>();
+
     // Start is called before the first frame update
     void Start()
     {
