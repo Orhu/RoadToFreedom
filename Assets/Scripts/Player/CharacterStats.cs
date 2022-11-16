0e0c21e9632ef85d19e18b51a84452c7384422b0
Orhu <mcdonoughmalcolm@gmail.com>
2022-11-15 19:35:35 -0500

    merge corrections

--- Assets/Scripts/Player/CharacterStats.cs
+++ Assets/Scripts/Player/CharacterStats.cs
@@ -12,57 +12,54 @@ public class CharacterStats : MonoBehaviour
     public int challengeScore{get; private set;}
 
     public List<string> status = new List<string>();  
-    public IDictionary<string, string> statusDescription = new IDictionary<string, string>();
+    public Dictionary<string, string> statusDescription = new Dictionary<string, string>();
 
-    void Start()
-    {
+    void Start() {
         food = 0;
         medicine = 0;
         money = 0f;
         challengeScore = 0;
     }
 
-    public void IntialHealth(int vit) //Sets Initial health to the player based on their vitality
-    {
+    public void IntialHealth(int vit) { //Sets Initial health to the player based on their vitality
         health = vit * 2;
         if(health < 1){ //If vitality is 0, we assign them the minimum amount of health
             health = 1;
         }
     }
 
-    public void InitialStatuses(List<string> st, IDictionary<string, string> stdesc)
-    {
+    public void InitialStatuses(List<string> st, Dictionary<string, string> stdesc) {
         status = st;
         statusDescription = stdesc;
     }
 
-    public void ChangeHealth(int boost){ //Changes health based on boost
-        health += boost
+    public void ChangeHealth(int boost) { //Changes health based on boost
+        health += boost;
     }
 
-    public void ChangeFood(int ration ){ //Changes food stored based on ration
+    public void ChangeFood(int ration) { //Changes food stored based on ration
         food += ration; 
     }
 
-    public void ChangeMedicine(int meds){ //Changes medicine based on meds
+    public void ChangeMedicine(int meds) { //Changes medicine based on meds
         medicine += meds;
     }
 
-    public void ChangeMoney(float cash){ //Changes money based on cash 
+    public void ChangeMoney(float cash) { //Changes money based on cash 
         money += cash;
     }
 
-    public void ChangeCS(int difficult){ //Changes challengeScore based on difficult
+    public void ChangeCS(int difficult) { //Changes challengeScore based on difficult
         challengeScore += difficult;
     }
 
-    public void ChangeStatuses(string st, string stdesc){ //Adds a status and it's description
+    public void AddStatus(string st, string stdesc) { //Adds a status and it's description
         status.Add(st);
         statusDescription.Add(st, stdesc);
     }
 
-    public void ChangeStatuses(string st) { //Overloaded function which only takes the status name and removes it from the list and description
+    public void RemoveStatus(string st) { //Overloaded function which only takes the status name and removes it from the list and description
         status.Remove(st);
-        statusDescription.Remove(st)
+        statusDescription.Remove(st);
     }
 }
