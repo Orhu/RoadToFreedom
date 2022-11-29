d3c8a07243f928fcb25faedd8dd026c98b42bd18
Riley Collins <collin_r2@denison.edu>
2022-11-29 18:05:01 -0500

    joe mama

--- Assets/Scripts/Events/eventHandler.cs
+++ Assets/Scripts/Events/eventHandler.cs
@@ -111,8 +111,8 @@ public class eventHandler : MonoBehaviour
     void Start() {
         allEvents = JsonUtility.FromJson<EventCollection>(eventJson.text);
 
-        _charStats = GameObject.Find("Player").GetComponent<CharacterStats>();
-        _charSheet = GameObject.Find("Player").GetComponent<CharacterSheet>();
+        // _charStats = GameObject.Find("Player").GetComponent<CharacterStats>();
+        // _charSheet = GameObject.Find("Player").GetComponent<CharacterSheet>();
         
         // Used to initialize event window for testing. May be altered late     
         gameObject.SetActive(false);
@@ -207,16 +207,10 @@ public class eventHandler : MonoBehaviour
                         break;
                     case 1: // Skill
                         if(s.effects[i].effectOperation == 'c'){ // Change
-<<<<<<< HEAD
-                            // switch(s.effects[i].effectValA){
-                            //     // ADD SKILL NUMBERS HERE
-                            // }
-=======
                             //switch(s.effects[i].effectValA){
                                 // ADD SKILL NUMBERS HERE
                                 
                             //}
->>>>>>> 7cc43c07d555214acdb102cd67394499874648b3
                         }
                         break;
                     case 2: // Resource
@@ -225,19 +219,22 @@ public class eventHandler : MonoBehaviour
                                 case 0: // health
                                     int changeH = s.effects[i].effectValB;
                                     // ADD HEALTH VALUE CHANGE HERE
+                                    CharacterStats.ChangeHealth(changeH);
                                     break;
                                 case 1: // food
                                     int changeF = s.effects[i].effectValB;
                                     // ADD FOOD VALUE CHANGE HERE
+                                    CharacterStats.ChangeFood(changeF);
                                     break;
                                 case 2: // medicine
                                     int changeMe = s.effects[i].effectValB;
                                     // ADD MEDICINE VALUE CHANGE HERE
+                                    CharacterStats.ChangeMedicine(changeMe);
                                     break;
                                 case 3: // money
                                     int changeMo = s.effects[i].effectValB;
                                     // ADD MONEY VALUE CHANGE HERE
-                                    
+                                    CharacterStats.ChangeMoney(changeMo);
                                     break;
                             }
                         }
@@ -271,8 +268,6 @@ public class eventHandler : MonoBehaviour
             Debug.LogError("Option not in valid range");
         }
         else{
-            //processAttribute(option);
-            // ADD CODE HERE LATER TO MARK IF CHANGES ARE SUCCESS CONDITION OR FAILURE
 
             // Sets buttons to close options and none other
             // text.text = curEvent.eventChoices[option].choicePassText;
