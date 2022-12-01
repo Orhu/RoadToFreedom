3bafa4c6561eac549774607846c25009f5641abe
Riley Collins <collin_r2@denison.edu>
2022-11-30 21:30:41 -0500

    stuck at buttons

---
+++ Assets/Scripts/Town/town.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class town : MonoBehaviour
+{
+    private static SceneController _control;
+    private static eventHandler _event;
+
+    // Text GameObjects
+    // [SerializeField] TMP_Text shopButtonText;
+
+    void Start(){
+        _event = GameObject.Find("eventUI").GetComponent<eventHandler>();
+    }
+
+    // Handled opening the shop event
+    public static void openShop(int shopID){
+        _control.StartEventLoad(shopID);
+    }
+
+
+}
