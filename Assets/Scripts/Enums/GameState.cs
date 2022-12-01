ed70bd67c99a629d9c5211334ddca06ad614e4cc
Orhu <mcdonoughmalcolm@gmail.com>
2022-12-01 18:40:21 -0500

    slave catchers + trail events json

--- Assets/Scripts/Enums/GameState.cs
+++ Assets/Scripts/Enums/GameState.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public enum GameState {
     CHARACTER_BUILDER,
     ON_TRAIL,
---
+++ Assets/Scripts/Enums/SlaveCatcherState.cs
@@ -0,0 +1,6 @@
+public enum SlaveCatcherState {
+    INACTIVE,
+    ON_TRAIL,
+    INVESTIGATING_TOWN,
+    FINDING_PLAYER
+}
