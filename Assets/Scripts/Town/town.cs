e40ec944a96c0300403fe55ec0b1611c99b3c57f
Riley Collins <collin_r2@denison.edu>
2022-12-06 19:06:26 -0500

    friend fix

--- Assets/Scripts/Town/town.cs
+++ Assets/Scripts/Town/town.cs
@@ -10,6 +10,8 @@ public class town : MonoBehaviour
 {
     private static eventHandler _event;
 
+    public static bool friend = true;
+
     // Text GameObjects
     // [SerializeField] TMP_Text shopButtonText;
 
@@ -25,6 +27,14 @@ public class town : MonoBehaviour
     // Closes out town and continues journey
     public static void continueJourney(){
         SceneController.LoadTrail();
+    }
+
+    // Prevent infinite money gen from friend
+    public static void plantationFriend(){
+        if (friend == true){
+            //openShop(102);
+            friend = false;
+        }
     }
 
 
