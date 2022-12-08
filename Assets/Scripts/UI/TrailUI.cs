fd4c66746bd72cad3e65c0f22c2075e8a3864a40
Orhu <mcdonoughmalcolm@gmail.com>
2022-12-07 19:57:38 -0500

    rough sky asset

--- Assets/Scripts/UI/TrailUI.cs
+++ Assets/Scripts/UI/TrailUI.cs
@@ -7,11 +7,13 @@ using TMPro;
 public class TrailUI : MonoBehaviour { 
     [SerializeField] TMP_Text timeText;
     [SerializeField] TMP_Text progressText;
+    [SerializeField] RectTransform skyImageTransform;
 
     void Update() {
         if (SceneController.gameState == GameState.ON_TRAIL) {
             timeText.text = $"Time: About {(int)World.time}:00";
             progressText.text = $"Progress: {Trail.progress}/{Trail.length}";
+            skyImageTransform.localRotation = Quaternion.Euler(0f,0f,Mathf.Lerp(360f, 0f, World.time/24f));
         }
     }
 }
