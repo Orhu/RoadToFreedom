8968136620a9b0829be5f7d5a3907d2a85b71e87
Riley Collins <collin_r2@denison.edu>
2022-12-08 00:48:01 -0500

    trees

--- Assets/Scripts/UI/TrailUI.cs
+++ Assets/Scripts/UI/TrailUI.cs
@@ -8,10 +8,12 @@ public class TrailUI : MonoBehaviour {
     [SerializeField] TMP_Text timeText;
     [SerializeField] TMP_Text progressText;
     [SerializeField] RectTransform skyImageTransform;
+    [SerializeField] RectTransform treesTransform;
 
     void Update() {
         if (SceneController.gameState == GameState.ON_TRAIL) {
             skyImageTransform.gameObject.SetActive(true);
+            treesTransform.gameObject.SetActive(true);
             timeText.text = $"Time: About {(int)World.time}:00";
             progressText.text = $"Progress: {Trail.progress}/{Trail.length}";
             skyImageTransform.localRotation = Quaternion.Euler(0f,0f,Mathf.Lerp(360f, 0f, World.time/24f));
@@ -19,6 +21,7 @@ public class TrailUI : MonoBehaviour {
             timeText.text = $"Time: About {(int)World.time}:00";
         } else {
             skyImageTransform.gameObject.SetActive(false);
+            treesTransform.gameObject.SetActive(false);
         }
     }
 }
