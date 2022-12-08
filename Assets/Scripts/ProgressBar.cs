77ad8056cf590a0e59a4ef4a4ee5679f9da4caaf
Kahf Hussain <kahfnhussain@gmail.com>
2022-12-08 00:36:21 -0500

    Complete Progress Bar

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 StartPosition; 
+    public float speed;
+    void Start()
+    {
+        StartPosition = transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Trail.trailNum == 0)
+        {
+            transform.position = new Vector3(Mathf.Lerp(StartPosition.x, StartPosition.x + 200, Trail.progress/45f), StartPosition.y, StartPosition.z);
+        }
+        else if(Trail.trailNum == 1)
+        {
+            transform.position = new Vector3(Mathf.Lerp(StartPosition.x, StartPosition.x + 200, Trail.progress/60f), StartPosition.y, StartPosition.z);
+        }
+        else if(Trail.trailNum == 2)
+        {
+            transform.position = new Vector3(Mathf.Lerp(StartPosition.x, StartPosition.x + 200, Trail.progress/90f), StartPosition.y, StartPosition.z);
+        }
+        else if(Trail.trailNum == 3)
+        {
+            transform.position = new Vector3(Mathf.Lerp(StartPosition.x, StartPosition.x + 200, Trail.progress/120f), StartPosition.y, StartPosition.z);
+        }
+        else if(Trail.trailNum == 4)
+        {
+            transform.position = new Vector3(Mathf.Lerp(StartPosition.x, StartPosition.x + 200, Trail.progress/120f), StartPosition.y, StartPosition.z);
+        }
+        else if(Trail.trailNum == 5)
+        {
+            transform.position = new Vector3(Mathf.Lerp(StartPosition.x, StartPosition.x + 200, Trail.progress/15f), StartPosition.y, StartPosition.z);
+        }
+                
+    }
+}
